e34eb354543e389065e7bd0277b1075d96ff10ed
MathManiacc <73884571+MathManiacc@users.noreply.github.com>
2020-11-25 15:08:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paper, dustbin1;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper(50, 600);
+
+	ground = new Ground(200, 672, 2000, 20);
+
+	dustbin1 = new Dustbin(550, 535, 230, 260);
+
+	Engine.run(engine);
+}
+
+function draw() {
+	background("lightgray");
+
+	paper.display();
+	ground.display();
+	dustbin1.display();
+
+	//paper.depth = dustbin1.depth;
+    //dustbin1.depth = dustbin1.depth + 1;
+
+	drawSprites();
+}
+
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body, paper.body.position, {x:205, y:-305});
+	}
+}
+
+
+
